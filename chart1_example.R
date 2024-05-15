40ba0f9539a0d7018805b8baf34bd0557c2c004b
LYZYZ <yl458@uw.edu>
2024-05-14 17:08:22 -0700

    added files

--- chart1_example.R
+++ chart1_example.R
@@ -1,7 +1,17 @@
-x_values <- seq(1, 3)
-y_values <- seq(1,3)
 
+data <- read.csv("movies.csv")
 library(ggplot2)
-ggplot() +
-  geom_point(aes(x=x_values, y = y_values))
+library(tidyr)
+
+
+data <- data.frame(lapply(data, function(x) if(is.character(x)) iconv(x, to = "UTF-8") else x))
+
+
+
+scatter_plot <- ggplot(data, aes(x = votes, y = score)) +
+  geom_point(color = "red") +
+  labs(title = "Scatter Plot of Votes vs Score",
+       x = "Votes",
+       y = "Score") +
+  theme_minimal()
 
